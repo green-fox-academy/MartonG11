229cab9ce8d897eb9b9e4ccd5639fc25ef29018c
MartonG11 <gabormarton91@gmail.com>
2017-11-28 16:42:14 +0100

    day2 80%

---
+++ week-02/day-2/increment_element.py
@@ -0,0 +1,12 @@
+# - Create a variable named `t`
+#   with the following content: `[1, 2, 3, 4, 5]`
+# - Increment the third element
+# - Print the third element
+
+t =[1, 2, 3, 4, 5]
+
+def inc(x):
+    print(x[3] + 1)
+
+inc(t)
+
