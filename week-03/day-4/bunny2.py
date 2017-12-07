960e0beb32319a252621b70326cc4a74386458b0
MartonG11 <gabormarton91@gmail.com>
2017-12-07 15:04:01 +0100

    w3d4 bunny2.py done

---
+++ week-03/day-4/bunny2.py
@@ -0,0 +1,19 @@
+# We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+# (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+# have 3 ears, because they each have a raised foot. Recursively return the
+# number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+# bunny_line: [1, 2, 3, 4, 5, .... ,n]
+# odd_bunny = 2 ears
+# even_bunny = 3 ears
+# print like this: [2 "ears", 3 "ears", 2, 3, 2, 3, 2 ....]
+
+def ear_calculator(bunnies):
+    if bunnies == 0:
+        return 0
+    elif bunnies % 2 == 0:
+        return 3 + ear_calculator((bunnies) - 1)
+    else:
+        return 2 + ear_calculator((bunnies) - 1)
+
+print(ear_calculator(2))
