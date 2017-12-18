10925b8ba51811eded0c063775f82fc7e2d131fc
MartonG11 <gabormarton91@gmail.com>
2017-12-18 17:26:29 +0100

    hero added to the map: visible, but not moving yet

--- week-05/THE_RPG_Game/main.py
+++ week-05/THE_RPG_Game/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from map_draw import Tiles
 
 root = Tk()
+root.title('DAMN RPG SO MUCH FUN')
 canvas = Canvas(root, width = 720, height = 720, bg="white", bd=0)
 
 map_draw = Tiles()
@@ -21,11 +22,20 @@ class Tile():
                 else:
                     canvas.create_image(x*72, y*72, anchor = NW, image = self.wall)
 
+class Hero(object):
+    def __init__(self):
+        self.Hero_down = PhotoImage(file = 'assets/hero-down.png')
+        self.draw_hero()
+
+    def draw_hero(self):
+        self.hero_img = canvas.create_image(0, 0, anchor = NW, image = self.Hero_down)
+
 
 
 
 canvas.pack()
-
 map_drawer = Tile()
+hero = Hero()
+
 canvas.focus_set()
 root.mainloop()
